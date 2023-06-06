5058a7d0bc2afdc33480ee6580d13b06af84640f
alidrisy <email@example.comclear>
2023-06-07 00:25:57 +0300

    7. islower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for num in range(0, 10):
-	for num1 in range(num+1, 10):
-		if num == 8 and num1 == 9:
-			break
-		print("{}".format(num % 10), end="")
-		print("{}".format(num1 % 10), end=", ")
+    for num1 in range(num+1, 10):
+        if num == 8 and num1 == 9:
+            break
+        print("{}".format(num % 10), end="")
+        print("{}".format(num1 % 10), end=", ")
 print("89")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def islower(c):
+    x = ord(c)
+    if x >= 97 and x <= 126:
+        return (1)
+    return(0)
