0955638319edd53cd95357a20678f130b250b80f
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-06-06 13:06:04 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,3 @@ else:
 		print(f"Last digit of {number:d} is {num:d} and is 0")
 	else:
 		print(f"Last digit of {number:d} is -{num:d} and is less than 6 and not 0")
-		
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for str in range(97, 123):
+for str in range(97,123):
 	print("{:s}".format(chr(str)), end="")
