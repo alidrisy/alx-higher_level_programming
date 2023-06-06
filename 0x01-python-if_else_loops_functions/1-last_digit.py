14e50495acf43ccc72e0ab65969ee946237ed950
alidrisy <email@example.comclear>
2023-06-06 23:36:23 +0300

    Hexadecimal printing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number > 0:
     if num > 5:
         print(f"Last digit of {number:d} is {num:d} and is greater than 5")
     else:
-        print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
-else:
+        print(f"Last digit of {number} is {num} and is less than 6 and not 0")
+else
     if num == 0:
         print(f"Last digit of {number:d} is {num:d} and is 0")
     else:
-        print(f"Last digit of {number:d} is -{num:d} and is less than 6 and not 0")
+        print(f"Last digit of {number} is -{num} and is less than 6 and not 0")
