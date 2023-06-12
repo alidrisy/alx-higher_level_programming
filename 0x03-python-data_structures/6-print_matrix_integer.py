68551b7246fb044fe9c55d4bb78555060bf09c6f
alidrisy <email@example.comclear>
2023-06-12 09:06:18 +0300

    fix it

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
     x = range(3)
     for i in x:
         for n in x:
-            print("{}".format(matrix[i][n]), end='')
-        print('')
+            print("{:d}".format(matrix[i][n]), end='')
+       print('')
