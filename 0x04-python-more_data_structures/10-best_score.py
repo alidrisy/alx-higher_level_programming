451a9d5c1b4abf8c9f2b9154309004541e4e53e2
alidrisy <email@example.comclear>
2023-06-16 22:40:42 +0300

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return (None)
+  n = 0
+    x = None
     for i, j in sorted(a_dictionary.items()):
-        x = i
+        if j > n:
+            n = j
+            x = i
     return x
