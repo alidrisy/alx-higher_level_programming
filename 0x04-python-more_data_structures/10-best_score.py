563938937392ac6a5c1a93dcc6c422327dde7ccd
alidrisy <email@example.comclear>
2023-06-16 22:46:43 +0300

    Multiply by using map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return (None)
-  n = 0
+    n = 0
     x = None
     for i, j in sorted(a_dictionary.items()):
         if j > n:
