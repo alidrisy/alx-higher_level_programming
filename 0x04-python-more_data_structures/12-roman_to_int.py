d4da8de43a776eff999bbf57f2c7bad90fa91c0a
alidrisy <email@example.comclear>
2023-06-17 23:32:00 +0300

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 	x = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    if not isinstance(roman_string, str):
+		return 0
 	s = 0
 	r = len(roman_string)
 	for i, j in x.items():
