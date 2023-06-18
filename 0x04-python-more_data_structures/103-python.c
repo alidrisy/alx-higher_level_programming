42b3c1a8fa4dda390d727e3e3babc356eef766fc
alidrisy <email@example.comclear>
2023-06-19 00:27:52 +0300

    add

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
+
+void print_python_bytes(PyObject *p)
+{
+Py_ssize_t bytsize, i;
+char *str;
+
+bytsize = PyBytes_Size(p);
+
+printf("[.] bytes object info size: %d", (int)bytsize);
+
+str = PyBytes_AsString(p);
+if (strcmp(p->ob_type->tp_name, "bytes")) 
+{
+printf(" [ERROR] Invalid Bytes Object\n"); 
+return; 
+}
+
+printf("   trying string: ")
+for (x = 0; str[x]; x++)
+printf("%s", str[x]);
+printf("\n");
+
+if (bytsize <= 10)
+{
+printf("   first %d bytes: ", (int)bytsize + 1);
+for (i = 0; i <= bytsize; i++)
+printf("%x ", (void*)str[i]);
+printf("\n");
+}
+else
+{
+printf("   first 10 bytes: ");
+for (i = 0; i < 10; i++)
+printf("%x ", (void*)str[i]);
+printf("\n");
+}
+}
