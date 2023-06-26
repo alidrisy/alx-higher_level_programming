d211cd0d0c20e7f67600525f85372d2f9b32b1e2
alidrisy <email@example.comclear>
2023-06-27 01:00:44 +0300

    Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (IndentationError, ValueError):
+    except (TypeError, IndentationError, ValueError):
         return False
