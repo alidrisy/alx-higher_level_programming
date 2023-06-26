38d5ff4e72ee44412fadf7eb3132269d213f4cc3
alidrisy <email@example.comclear>
2023-06-27 01:04:09 +0300

    Print and count integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end='')
             except IndexError:
                 print()
-                return(n)
+                return n
             else:
                 n += 1
     print()
-    return (n)
+    return n
