b728036cfc5ad70e70c24bb01ff38b710069713f
alidrisy <email@example.comclear>
2023-06-28 22:46:22 +0300

    fix bycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represint a class square."""
-	
+
     def __init__(self, size=0):
         """Initialize a new square.
 
@@ -16,7 +16,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-	
+
     def area(self):
         """Returns the current square area."""
         return (self.__size * self.__size)
