1a33d266c047a09cb5e6a5d58ea2a13999600903
alidrisy <email@example.comclear>
2023-07-04 22:19:37 +0300

    add it

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define text_indentation function"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after characters: ., ? and':'."""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    text = text.replace("? ", "?\n\n")
+    text = text.replace(": ", ":\n\n")
+    text = text.replace(". ", ".\n\n")
+    print(text, end="")
