b36149f9ca2310f5ac2a16cd7bb95fffcb017b91
alidrisy <email@example.comclear>
2023-07-05 19:26:07 +0300

    fix error

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Define text_indentation function"""
+""" Doc """
 
 
-def text_indentation(text):
-    """prints a text with 2 new lines after characters: ., ? and':'."""
+def text_indentation(text=""):
+    """ Doc """
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    text = text.replace("? ", "?\n\n")
-    text = text.replace(":    ", ":\n\n")
-    text = text.replace(": ", ":\n\n")
-    text = text.replace(".", ".\n\n")
-    text = text.replace(".\n\n ", ".\n\n")
-    print(text, end="")
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
