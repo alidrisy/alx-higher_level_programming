5afbb7ea17e5f3585d33488c345013418c535dd3
alidrisy <email@example.comclear>
2023-07-10 23:09:06 +0300

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define non empty class"""
+
+
+class BaseGeometry:
+    """A raise error class"""
+
+    def area(self):
+        """Raises an Exception with the message"""
+        raise Exception("area() is not implemented")
