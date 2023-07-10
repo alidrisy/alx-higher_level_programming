d3b8a79d0cca02a9866e02eb613b19a8057bdb9a
alidrisy <email@example.comclear>
2023-07-10 21:48:34 +0300

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define MyList class"""
+
+
+class MyList(list):
+    """ inherits from list"""
+
+    def __init__(self):
+        """Initialize data"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the list"""
+        list1 = super().__str__()
+        new = []
+        for i in list1:
+            if i.isdigit():
+                new.append(int(i))
+            print(sorted(new))
