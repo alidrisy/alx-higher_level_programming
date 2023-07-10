5877a157cacaebcff49e7ae714c25cd3cbf8935d
alidrisy <email@example.comclear>
2023-07-11 00:19:48 +0300

    fixing errors

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialize data"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+        super().integer_validator("height", height)
+		super().integer_validator("width", width)
