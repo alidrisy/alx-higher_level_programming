f24555dc7112c834af5cd4c2fe9b00681d5f56c4
alidrisy <email@example.comclear>
2023-07-11 01:23:33 +0300

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize data"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,5 +18,4 @@ class Rectangle(BaseGeometry):
         return self.__height * self.__width
 
     def __str__(self):
-        return "[{}] {}/{}".\
-                format(self.__class__.__name__, self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
