10c18ba95407972eb667cd516fbf2e8186a16ff1
alidrisy <email@example.comclear>
2023-07-11 04:35:59 +0300

    13. Can I?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define add_attribute function"""
+
+
+def add_attribute(cls, name, new):
+    """adds a new attribute to an object if it’s possible to class"""
+    if not hasattr(cls, "__dict__"):
+        raise TypeError("can't add new attribute")
+    cls.name = new
