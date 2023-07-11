6bfad1a9f2d66af62c13d0199a0769944755d0e3
alidrisy <email@example.comclear>
2023-07-11 21:31:28 +0300

    fix errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, encoding="utf-8") as fp:
-        print(fp.read())
+        print(fp.read(), end="")
