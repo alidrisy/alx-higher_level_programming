2bb32077c38bd8ae84c3ef0195f99e5e6d8b89de
alidrisy <email@example.comclear>
2023-07-12 00:47:07 +0300

    fix errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,12 +3,11 @@
 and then save them to a file"""
 
 
-
 if __name__ == "__main__":
     argv = __import__('sys').argv
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    
+    load_from_json_file = __import__('6-load_from_json_file').\
+        load_from_json_file
 
     filename = "add_item.json"
     try:
