58beeaa2b2b5a63aad6ce8373596cc68b5d61319
alidrisy <email@example.comclear>
2023-07-11 21:25:24 +0300

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define read_file function"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, encoding="utf-8") as fp:
+        print(fp.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,6 @@
+This derectory was created for the Python - Input/Outputproject.
+In this project we will learn:
+- How to open a file
+- How to write text in a file
+- How to read the full content of a file
+- and so on...
