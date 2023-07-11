3493492cf9346e92f4c85a1aef3693dfe4322d71
alidrisy <email@example.comclear>
2023-07-11 23:04:34 +0300

    Fix style error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-json = __import__('json')
 """Define save_to_json_file function"""
+json = __import__('json')
 
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
 
-    with open(filename, mode="w", encoding="utf-8") as fp:
+    with open(filename, encoding="utf-8") as fp:
         json.dump(my_obj, fp)
