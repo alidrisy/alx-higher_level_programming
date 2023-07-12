bc7674717df20e1fae161eeb994a2a977e6aff4e
alidrisy <email@example.comclear>
2023-07-12 18:20:00 +0300

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new = []
+            for key in attrs:
+                if type(key) is str and key in self.__dict__.keys():
+                    new += [(key, getattr(self, key))]
+            return dict(new)
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i, j in json.items():
+            setattr(self, i, j)
