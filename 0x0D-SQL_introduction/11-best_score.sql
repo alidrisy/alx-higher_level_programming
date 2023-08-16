f824a3c1391acffaaae45d623d30c5fc5ac3b072
alidrisy <a.alidrisy6@gmail.com>
2023-08-16 17:04:15 +0300

    Cheating is bad

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0
+-- lists all records with a score >= 10 in the table second_table
 SELECT score, name
 FROM second_table
 WHERE score >= 10
