c18e99d9bd5fafe6d55ead1bc5e52a9cbc3e0dc4
alidrisy <a.alidrisy6@gmail.com>
2023-08-16 22:34:33 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRNAT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRNAT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
