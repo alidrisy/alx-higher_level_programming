3077e109632cba0ee997712c7f4eca6263acd8e7
alidrisy <a.alidrisy6@gmail.com>
2023-08-17 17:06:04 +0300

    List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,5 +3,4 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
