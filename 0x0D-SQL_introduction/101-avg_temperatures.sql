6366658e9cfa266e2d146ea89ac900687e4bf119
alidrisy <a.alidrisy6@gmail.com>
2023-09-04 00:50:41 +0300

    18. Temperatures #0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Import in hbtn_0c_0 database a table
-SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
