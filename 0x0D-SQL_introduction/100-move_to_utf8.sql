45c8c1627b54e61bed4119febe133fd334841aa0
alidrisy <a.alidrisy6@gmail.com>
2023-09-04 00:25:41 +0300

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
-CONVERT name TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
