79697ad9cceb30192b3c8c8dc2bc8a2141413d4e
alidrisy <a.alidrisy6@gmail.com>
2023-09-07 22:48:49 +0300

    Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- This script lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows, tv_show_ratings
+WHERE tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
