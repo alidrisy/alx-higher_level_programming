ed09d377e8c93b4e6956bb98dfabedf92d08993a
alidrisy <a.alidrisy6@gmail.com>
2023-09-12 01:38:06 +0300

    fix errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (length <= 3) {
 } else {
   const myList = [];
   for (let i = 0; i < length - 2; i++) {
-    myList[i] = Number(process.argv[i + 2]);
+    myList[i] = parseInt(process.argv[i + 2]);
   }
   myList.sort();
   myList.reverse();
