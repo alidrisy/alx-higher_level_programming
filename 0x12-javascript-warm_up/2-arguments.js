07a2d76cd613d841ebdbef080a3cabf41219035a
alidrisy <a.alidrisy6@gmail.com>
2023-09-11 22:32:04 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
-	console.log("No argument");
-} else {
-	console.log("Argument found");
+	console.log('No argument');
+}
+else if (process.argv.length === 3) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
 }
