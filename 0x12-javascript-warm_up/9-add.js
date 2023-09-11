8cfaabdcb36243a31d5fe593115957c3d29a0270
alidrisy <a.alidrisy6@gmail.com>
2023-09-12 00:24:00 +0300

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,5 +4,9 @@ const num2 = Number(process.argv[3]);
 if (isNaN(num1) || isNaN(num2)) {
   console.log('NaN');
 } else {
-  console.log(num1 + num2);
+  console.log(add(num1 ,num2));
+}
+
+function add(a, b) {
+  return a + b;
 }
