9fe5862db44e7448cd05342eac4140869839d2de
alidrisy <a.alidrisy6@gmail.com>
2023-09-13 00:12:33 +0300

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,10 +2,6 @@
 
 exports.converter = function (base) {
   return function (num) {
-    if (isNaN(parseInt(num.toString(base)))) {
       return num.toString(base);
-    } else {
-      return parseInt(num.toString(base));
-    }
   };
 };
