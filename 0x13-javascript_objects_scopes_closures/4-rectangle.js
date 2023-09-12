8a1b76a05160bfec866c6df819b14b4087e41629
alidrisy <a.alidrisy6@gmail.com>
2023-09-12 22:14:38 +0300

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!(!w || !h || w === 0 || h === 0 || h < 0 || w < 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0, str; i < this.height; i++) {
+      str = '';
+      for (let x = 0; x < this.width; x++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const hight = this.height;
+    this.height = this.width;
+    this.width = hight;
+  }
+
+  double () {
+    this.height *= this.height;
+    this.width *= this.width;
+  }
+};
