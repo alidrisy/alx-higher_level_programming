b79b81f5eb95f20dbf12ed8ff438a404bfc92151
alidrisy <a.alidrisy6@gmail.com>
2023-09-13 23:12:18 +0300

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const list1 = list.map((e) => e * list.indexOf(e));
+const list1 = list.map((e, index) => e * index);
 console.log(list1);
