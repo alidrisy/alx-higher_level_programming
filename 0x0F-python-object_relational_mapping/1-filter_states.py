9bb911292a46e282cd5724a86b93de427b89cd2f
alidrisy <a.alidrisy6@gmail.com>
2023-09-15 20:29:07 +0300

    fix errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     con = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                           passwd=argv[2], db=argv[3], charset="utf8")
     curs = con.cursor()
-    curs.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id")
+    curs.execute("SELECT * FROM states WHERE CONVERT(`name` USING Latin1)\
+            COLLATE Latin1_General_CS LIKE 'N%' ORDER BY id")
     rows = curs.fetchall()
 
     for row in rows:
