9816c342652764054a158f99b31bb0baaf30b81a
alidrisy <a.alidrisy6@gmail.com>
2023-09-16 18:22:07 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,9 +10,11 @@ from sqlalchemy.orm import sessionmaker, relationship
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    State.cities = relationship("City", order_by=City.id, back_populates='states')
+    State.cities = relationship("City", order_by=City.id,
+                                back_populates='states')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state, city in session.query(State, City).filter(State.id == City.state_id).order_by(City.id):
+    for state, city in session.query(State, City).filter(
+            State.id == City.state_id).order_by(City.id):
         print('{}: ({}) {}'.format(state.name, city.id, city.name))
