45174ed25cc90b46a980094311baf66b6ebf74e7
alidrisy <a.alidrisy6@gmail.com>
2023-09-16 20:35:16 +0300

    List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).outerjoin(City).order_by(State.id, City.id):
+    for state in session.query(State).outerjoin(City).order_by(State.id, City.id).all():
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
