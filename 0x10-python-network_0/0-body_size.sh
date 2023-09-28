ad8d2773224bea77a4fae0e0d7d10b82804e5fad
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-09-28 13:04:51 -0700

    fix error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-x=$(curl -Is "$1" | grep Content-Length)
-s=$(echo "$x" | cut --complement -d " " -f1)
-echo "$s"
+echo "$(echo "$(curl -Is "$1" | grep Content-Length)" | cut --complement -d " " -f1)"
