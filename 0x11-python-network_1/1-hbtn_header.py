c5fa2e6917b551cbbc68717c718bb8f70e59de24
alidrisy <a.alidrisy6@gmail.com>
2023-09-29 21:12:26 +0300

    0. Whats my status? #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Model to fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+ur = request.Request('https://alx-intranet.hbtn.io/status')
+with request.urlopen(ur) as req:
+    str1 = req.read()
+print("Body response:")
+print(f"   - type: {type(str1)}")
+print(f"   - content: {str1}")
+print(f"   - utf8 content: {str1.decode('ascii')}")
