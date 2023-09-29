d41ba67cd66ec9815db29d397a41a82db9912308
alidrisy <a.alidrisy6@gmail.com>
2023-09-29 21:23:37 +0300

    0. Whats my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """ Model to fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 
-
-ur = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(ur) as req:
-    str1 = req.read()
-print("Body response:")
-print(f"   - type: {type(str1)}")
-print(f"   - content: {str1}")
-print(f"   - utf8 content: {str1.decode('ascii')}")
+if __name__ == "__main__":
+    ur = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(ur) as req:
+        str1 = req.read()
+    print("Body response:")
+    print(f"   - type: {type(str1)}")
+    print(f"   - content: {str1}")
+    print(f"   - utf8 content: {str1.decode('ascii')}")
