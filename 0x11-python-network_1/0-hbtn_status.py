2623dd3be5af2f2efbb01878f1959648ed84aef1
alidrisy <a.alidrisy6@gmail.com>
2023-09-29 21:55:00 +0300

    fix errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """ Model to fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 
+
 if __name__ == "__main__":
     ur = request.Request('https://alx-intranet.hbtn.io/status')
     with request.urlopen(ur) as req:
         str1 = req.read()
-    print("Body response:")
-    print(f"    - type: {type(str1)}")
-    print(f"    - content: {str1}")
-    print(f"    - utf8 content: {str1.decode('ascii')}")
+        print("Body response:")
+        print(f"\t- type: {type(str1)}")
+        print(f"\t- content: {str1}")
+        print(f"\t- utf8 content: {str1.decode('ascii')}")
