d8fd2ba4a91dbbd843f1287ea8d7ca33ac4240d3
alidrisy <a.alidrisy6@gmail.com>
2023-09-30 18:24:36 +0300

    8. cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -H "Content-Type: application/json" -X POST --data $'cat "$2"' "$1"
+curl -sH "Content-Type: application/json" -X POST --data $"cat $2" "$1"
