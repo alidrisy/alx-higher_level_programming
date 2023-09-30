59630f4a6881d7302c3c3c343ba7bdafc32f33b1
alidrisy <a.alidrisy6@gmail.com>
2023-09-30 10:56:53 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -LX PUT -i 0.0.0.0:5000/catch_me -H "Origin:School"
+curl -LX PUT -s 0.0.0.0:5000/catch_me_3 -H "Origin:School"
