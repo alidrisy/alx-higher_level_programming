15289cb3bf610f10a9090b82c14df444745dd2f4
alidrisy <a.alidrisy6@gmail.com>
2023-10-24 19:42:56 +0300

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Reads and prints the content of a file.
+
+const fs = require('fs');
+const filename = process.argv[2];
+try {
+  const content = fs.readFileSync(filename, 'utf-8');
+  console.log(content);
+} catch (error) {
+  console.log(error);
+}
