c17905d76b9e5b638ee9e57c1526ced23f2b641e
alidrisy <a.alidrisy6@gmail.com>
2023-10-24 23:14:18 +0300

    fix error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,16 @@
 // Display the status code of a GET request.
 
 const request = require('request');
-const url = process.argv[2] + '?completed=true';
+const url = process.argv[2]
 request.get(url, function (_error, response) {
-  const charecter = JSON.parse(response.body);
+  const todo = JSON.parse(response.body);
+  const complete = []
+  todo.forEach((s) => s.completed === true ? complete.push(s) : 0);
   const dict1 = {};
   let i = 1;
   while (true) {
     const x = [];
-    charecter.forEach((s) => s.userId === i ? x.push(s) : 0);
+    complete.forEach((s) => s.userId === i ? x.push(s) : 0);
     if (x.length !== 0) {
       dict1[i] = x.length;
     } else {
